9006f63d39696052780caba2796008246e14bae4
Jake G <jake098877@gmail.com>
2020-04-28 00:59:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,7 @@ end
 
 def take_a_number(deli, name)
   if deli.length == 0
+    deli << name
     puts "Welcome, #{name}. You are number #{deli.length + 1} in line."
   elsif name.length == 1
     puts "Welcome, #{name}. You are number #{deli.length + 1} in line."
